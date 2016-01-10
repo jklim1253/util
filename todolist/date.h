f2beebb864130ef961c2b600a897494bdf975bab
Jong Kyu, Lim <jklim1253@gmail.com>
2016-01-10 23:45:59 +0900

    todolist requirement writing...

---
+++ todolist/date.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class date {
+private :
+public :
+};
---
+++ todolist/main.cpp
@@ -0,0 +1,16 @@
+#include "todolist.h"
+
+int main(int argc, char* argv[]) {
+/*
+wanted functionality.
+- add new todo
+	- property : contents, is-repeat, date-created
+- search todo
+- check todo
+- delete todo
+- get todo list
+
+*/
+
+	return 0;
+}
