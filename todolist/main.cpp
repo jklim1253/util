d663352b264eedd8a6076cedc031a89271f86ee0
Jong Kyu, Lim <jklim1253@gmail.com>
2016-01-10 23:53:32 +0900

    todo list requirement writing...

--- todolist/main.cpp
+++ todolist/main.cpp
@@ -4,12 +4,25 @@ int main(int argc, char* argv[]) {
 /*
 wanted functionality.
 - add new todo
-	- property : contents, is-repeat, date-created
+	- property : contents, is-repeat, date-created, alarm
+	- common-property : priority(0~9),
 - search todo
+	- search by date
+	- search by keyword
+	- search by category
 - check todo
+	- done
+	- refuse
+	- pause
 - delete todo
+	- to waste
+	- forever
 - get todo list
-
+	- count of items of todo list.
+	- top N items of todo list
+- interface
+	- dialog way
+	- response to request
 */
 
 	return 0;
